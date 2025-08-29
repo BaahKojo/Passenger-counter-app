4f10b67e63629689b8df52e406a862bad4c5a026
Baah Kojo <emmanuelbaah768@gmail.com>
2025-08-29 04:12:59 +0000

    Add files via upload

---
+++ Passenger counter app/index.js
@@ -0,0 +1,18 @@
+let count = 0
+let saveEl = document.getElementById('save-el');
+console.log('save-el');
+let countEl = document.getElementById('count-el')
+console.log('count-el')
+
+function increment() {
+    count = count + 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + '-'
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+
+}
